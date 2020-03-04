551d41d72ff1ae368a4046626016a8897948869d
Mateusz Belka <mateusz.belka14@gmail.com>
2020-03-04 06:59:47 +0100

    added items functionality

--- src/app/add-todo/add-todo.component.ts
+++ src/app/add-todo/add-todo.component.ts
@@ -6,19 +6,37 @@ import { Todo } from "./../shared/todo.class";
   styleUrls: ["./add-todo.component.scss"]
 })
 export class AddTodoComponent implements OnInit {
-  todos: Todo[] = [new Todo("chuja")];
+  todos: Todo[] = [new Todo("Coś do zrobienia")];
+  completeTodo: Todo[] = [];
   title = "";
   category: string;
-
-
+  life;
+  work;
+  school;
   constructor() {}
 
   ngOnInit(): void {}
   onAdd() {
     if (this.title !== "") {
       let todo = new Todo(this.title);
+      todo.items = [];
+      if(this.life) {
+        todo.items.push("Życie prywatne");
+      }
+      if(this.work) {
+        todo.items.push("Praca");
+      }
+      if(this.school) {
+        todo.items.push("Szkoła");
+      }
       console.log(todo);
       this.todos.push(todo);
     }
+  }
+  checkboxChangeHandler(i) {
+    let todo: Todo = this.todos[i];
+    this.todos.splice(i, 1);
+    todo.completeDate = Date.now();
+    this.completeTodo.push(todo);
   }
 }
--- src/app/shared/todo.class.ts
+++ src/app/shared/todo.class.ts
@@ -2,9 +2,12 @@ export class Todo {
     title: string
     completed: boolean
     createDate
+    completeDate
+    items: String[]
     constructor(title) {
         this.title = title,
         this.createDate = Date.now();
+        this.items = [];
     }
 
 }
