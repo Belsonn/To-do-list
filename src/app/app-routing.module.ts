e132c27b4528e1e7b049dc0fcc58e499736e911d
Mateusz Belka <mateusz.belka14@gmail.com>
2020-03-07 03:31:46 +0100

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CompletedComponent } from './completed/completed.component';
+import { AddTodoComponent } from './add-todo/add-todo.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'completed', component: CompletedComponent},
+  {path: '', component: AddTodoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
